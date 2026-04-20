77c0f874a5e721b41e59d3226abe8780c93fa03a
goatlens <86495037+goatlens@users.noreply.github.com>
2026-04-19 21:53:23 -0400

    Add theme toggle button for dark mode

---
+++ script.js
@@ -0,0 +1,11 @@
+let button = document.getElementById("themeButton");
+
+button.addEventListener("click", function () {
+  document.body.classList.toggle("dark-mode");
+
+  if (document.body.classList.contains("dark-mode")) {
+    button.textContent = "Light Mode";
+  } else {
+    button.textContent = "Dark Mode";
+  }
+});
